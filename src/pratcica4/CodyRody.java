7028fceb728ecc15eeef064d2138c3b285bf48f8
migue <migue@PC-19>
2019-12-11 14:47:45 +0100

    11/12 14:47

--- src/pratcica4/CodyRody.java
+++ src/pratcica4/CodyRody.java
@@ -3,7 +3,9 @@ package pratcica4;
 public class CodyRody {
 
 	final static int TAM_TABLERO = 5;
-	
+
+	final static String robotJuega = new String("Cody");
+
 	static int barrera1X = 0, barrera1Y = 0, barrera2X = 0, barrera2Y = 0;
 
 	static int coordXCody = 0, coordYCody = 0, coordXRody = 0, coordYRody = 0;
@@ -12,21 +14,31 @@ public class CodyRody {
 
 		boolean ganador = false;
 		int dado = 0;
-		String robotJuega = new String(""); // Cody - Rody
 
 		System.out.println("Bienvenido al juego de Cody & Rody. ");
 		System.out.println("Cuantas caras es el dado con el que se va a jugar: ");
-		// dado = Entrada.entero();
-		//
-		// while (!ganador) {
-		//
-		// }
 
-		System.out.println("La coordenada es: " + coordenadaAleatoria());
-		System.out.println(" B1X= "+barrera1X +", B1Y= " + barrera1Y +" B2X= " + barrera2X +" B2Y= " + barrera1Y);
+		dado = Entrada.entero();
+
+		coordXCody = coordenadaAleatoria();
+		coordYCody = coordenadaAleatoria();
+		coordXRody = coordenadaAleatoria();
+		coordYRody = coordenadaAleatoria();
+
+		while (coordXRody == coordXCody && coordYRody == coordYCody) {
+			coordXRody = coordenadaAleatoria();
+			coordYRody = coordenadaAleatoria();
+		}
+
 		asignaValoresBarreras();
-		System.out.println(" B1X= "+barrera1X +", B1Y= " + barrera1Y +" B2X= " + barrera2X +" B2Y= " + barrera1Y);
-		
+		System.out.println(
+				" Bar1X = " + barrera1X + ", Bar1Y = " + barrera1Y + " Bar2X = " + barrera2X + " Bar2Y = " + barrera1Y);
+		System.out.println(" CodyX = " + coordXCody + ", CodyY = " + coordYCody + " RodyX = " + coordXRody + " RodyY = "
+				+ coordYRody);
+
+		System.out.println();
+
+		mostrarTablero();
 	}
 
 	public static int coordenadaAleatoria() {
@@ -43,8 +55,21 @@ public class CodyRody {
 
 		barrera1X = coordenadaAleatoria();
 		barrera1Y = coordenadaAleatoria();
-		
-		while (barrera1X == barrera2X && barrera1Y == barrera2Y) {
+
+		while ((barrera1X == coordXCody && barrera1Y == coordYCody)
+				|| (barrera1X == coordXRody && barrera1Y == coordYRody)) {
+
+			barrera1X = coordenadaAleatoria();
+			barrera1Y = coordenadaAleatoria();
+		}
+
+		barrera2X = coordenadaAleatoria();
+		barrera2Y = coordenadaAleatoria();
+
+		while ((barrera2X == coordXCody && barrera2Y == coordYCody)
+				|| (barrera2X == coordXRody && barrera2Y == coordYRody)
+				|| (barrera1X == barrera2X && barrera1Y == barrera2Y)) {
+
 			barrera2X = coordenadaAleatoria();
 			barrera2Y = coordenadaAleatoria();
 		}
@@ -52,14 +77,50 @@ public class CodyRody {
 
 	public static void mostrarTablero() {
 
+		int cuadrado = TAM_TABLERO * 6;
+
+		for (int x = 0; x <= cuadrado; x++) {
+
+			for (int y = 0; y <= cuadrado; y++) {
+
+				if (x % 6 == 0 || y % 6 == 0) {
+					System.out.print(" *");
+
+				} else {
+					if (x == coordXCody && y == coordXCody) {
+						System.out.print(" C");
+					} else {
+						if (x == coordXRody && y == coordXRody) {
+							System.out.print(" R");
+						} else {
+
+							if (x == barrera1X && y == barrera1Y) {
+								System.out.print(" B");
+
+							} else {
+								if (x == barrera2X && y == barrera2Y) {
+									System.out.print(" B");
+
+								} else {
+									System.out.print("  ");
+								}
+
+							}
+						}
+					}
+				}
+			}
+			System.out.println();
+		}
 	}
 
-	public static int tirarDado(int numTotalDado) {
-		return 1;
+	public static int tirarDado(int dado) {
+		return (int) Math.floor(Math.random() * (dado - 1 + 1) + (1));
 	}
 
 	// <MOVIMIENTOS>
 	public static boolean moverArriba(String robotJuega) {
+
 		return true;
 	}
 
